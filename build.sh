00be04db7dd3181b850df3f2b4bb7f4acd4126ba
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-07 17:29:12 -0300

    Subfolders

--- build.sh
+++ build.sh
@@ -10,35 +10,35 @@ while [ -n "$1" ]; do
 	shift
 done
 
-COMPILE_NAME="helloWorld"
-COMPILE_FOLDER="build"
-COMPILE_PATH="${COMPILE_FOLDER}/${COMPILE_NAME}"
-PYTHON_EXEC="helloWorld"
+BUILD_PATH="build"
+COMPILE_PATH="${BUILD_PATH}/helloWorld"
+CPP_PATH="src/helloWorld"
+PYTHON_PATH="src/helloWorld"
 
-mkdir -p ${COMPILE_FOLDER}
+mkdir -p ${BUILD_PATH}
 
 # Shared Library Only
 if [[ ${compile} == "true" && ${run} != "true" ]]
 then
-    g++ -shared -fPIC -Wl,-install_name,${COMPILE_PATH}.so -o ${COMPILE_PATH}.so ${COMPILE_NAME}.cpp && python3 ${PYTHON_EXEC}.py
+    g++ -shared -fPIC -Wl,-install_name,${COMPILE_PATH}.so -o ${COMPILE_PATH}.so ${CPP_PATH}.cpp && python3 ${PYTHON_PATH}.py
 
 
 # Compile and RUN C++ Only
 elif [[ ${run} == "true" && ${compile} != "true" ]]
 then
-    g++ helloWorld.cpp -o ${COMPILE_PATH} && ./${COMPILE_PATH}
+    g++ ${CPP_PATH}.cpp -o ${COMPILE_PATH} && ./${COMPILE_PATH}
 
 
 # Shared Library And Compile/Run C++
 elif [[ ${run} == "true" && ${compile} == "true" ]]
 then
-    g++ -shared -fPIC -Wl,-install_name,${COMPILE_PATH}.so -o ${COMPILE_PATH}.so ${COMPILE_NAME}.cpp && python3 ${PYTHON_EXEC}.py
-    g++ helloWorld.cpp -o ${COMPILE_PATH} && ./${COMPILE_PATH}
+    g++ -shared -fPIC -Wl,-install_name,${COMPILE_PATH}.so -o ${COMPILE_PATH}.so ${CPP_PATH}.cpp && python3 ${PYTHON_PATH}.py
+    g++ ${CPP_PATH}.cpp -o ${COMPILE_PATH} && ./${COMPILE_PATH}
 
 
 # Clears Build Folder
 elif [[ ${delete} == "true" ]]
 then
     echo -e "\n Cleaning Build Folder \n";
-    rm ${COMPILE_FOLDER}/*
+    rm ${BUILD_PATH}/*
 fi;
